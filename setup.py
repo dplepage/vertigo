78bb3ecb5fe36b2964223a17e927d208b2087777
Daniel Lepage <dplepage@gmail.com>
2014-10-18 16:54:36 -0400

    Add pypi description that's shorter than README.rst.

--- setup.py
+++ setup.py
@@ -9,7 +9,19 @@ setup(
     author="Daniel Lepage",
     author_email="dplepage@gmail.com",
     packages=['vertigo',],
-    long_description=open('README.rst').read(),
+    long_description="""
+=========================================
+ Vertigo: Some really simple graph tools
+=========================================
+
+Vertigo is a small collection of classes and functions for building and working
+with graphs with labeled edges. This is useful because dictionaries are just
+graphs with labeled edges, and objects in Python are just dictionaries, so
+really this applies to pretty much all objects.
+
+See README.rst for more info
+
+""",
     url='https://github.com/dplepage/vertigo',
     classifiers=[
         "Development Status :: 3 - Alpha",
